aa40176eaad4777c1fab11ddf77e4ecae1c3a8aa
martindotexe <martin@solhaugfoto.no>
2024-11-06 01:41:10 +0100

    Day 4: Improvments. Removed math.power, with pointer magic

--- puzzles/day04/solution.go
+++ puzzles/day04/solution.go
@@ -2,7 +2,6 @@ package day04
 
 import (
 	"iter"
-	"math"
 	"strings"
 
 	"martindotexe/aoc/internal/utils"
@@ -16,20 +15,24 @@ func Run() (int, int) {
 
 func Part1(in iter.Seq[string]) int {
 	sum := 0
+	winningMap := make(map[int]bool)
 	for line := range in {
 		winning, numbers := parse(line)
-		winningMap := map[int]bool{}
 		for _, win := range winning {
 			winningMap[win] = true
 		}
 
-		winningTickets := 0
+		// Start with 1 point, double for each winning number
+		points := 1
 		for _, number := range numbers {
 			if winningMap[number] {
-				winningTickets++
+				points <<= 1
 			}
 		}
-		sum += int(math.Pow(2, float64(winningTickets-1)))
+		// Right shift to remove the initial 1 point
+		sum += points >> 1
+
+		clear(winningMap)
 
 	}
 	return sum
@@ -41,24 +44,29 @@ func Part2() int {
 
 func parse(s string) ([]int, []int) {
 	s = strings.Split(s, ": ")[1]
-	parts := strings.Split(s, "|")
-	out := [][]int{}
-	for _, part := range parts {
-		numbers := []int{}
-		currentNumber := 0
-		for p := range part {
-			if !isDigit(part[p]) {
-				continue
-			}
-			currentNumber = currentNumber*10 + int(part[p]-'0')
-			if p+1 >= len(part) || !isDigit(part[p+1]) {
-				numbers = append(numbers, currentNumber)
-				currentNumber = 0
-			}
+	numbers := []int{}
+	winningNumbers := []int{}
+	currentNumber := 0
+
+	processNumber := func() {
+		if currentNumber != 0 {
+			numbers = append(numbers, currentNumber)
 		}
-		out = append(out, numbers)
+		currentNumber = 0
 	}
-	return out[0], out[1]
+
+	for i := 0; i < len(s); i++ {
+		if isDigit(s[i]) {
+			currentNumber = currentNumber*10 + int(s[i]-'0')
+		} else if s[i] == '|' {
+			winningNumbers = numbers
+			numbers = []int{}
+		} else {
+			processNumber()
+		}
+	}
+	processNumber()
+	return winningNumbers, numbers
 }
 
 func isDigit(char byte) bool {
