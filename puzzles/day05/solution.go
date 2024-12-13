d9fd4b29ea2771a665896429607c302a82c47d37
martindotexe <martin@solhaugfoto.no>
2024-12-13 14:40:38 +0100

    Day 5: Use inbuilt sort

--- puzzles/day05/solution.go
+++ puzzles/day05/solution.go
@@ -1,6 +1,7 @@
 package day05
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 
@@ -12,115 +13,58 @@ func Run() (int, int) {
 	return part1(input), part2(input)
 }
 
-func checkPages(pages []int, rules map[int][]int) bool {
-	illegal := map[int]bool{}
-
-	for _, page := range pages {
-		if illegal[page] {
-			return false
-		}
-		if rule, ok := rules[page]; ok {
-			for _, r := range rule {
-				illegal[r] = true
-			}
-		}
-	}
-
-	return true
-}
-
-func parseRules(input []string) (map[int][]int, int) {
-	rules := map[int][]int{}
+func parseRules(input []string) (map[[2]int]bool, int) {
+	rules := make(map[[2]int]bool)
 	var i int
-	for i = 0; i < len(input); i++ {
-		if input[i] == "" {
-			break
-		}
+	for i = 0; i < len(input) && input[i] != ""; i++ {
 		line := input[i]
 		nums := strings.Split(line, "|")
 
 		a, _ := strconv.Atoi(nums[0])
 		b, _ := strconv.Atoi(nums[1])
 
-		rules[b] = append(rules[b], a)
+		rules[[2]int{a, b}] = true
+		rules[[2]int{b, a}] = false
 	}
-	return rules, i
+	return rules, i + 1
 }
+
+type sortByRule struct {
+	rules map[[2]int]bool
+	objs  []int
+}
+
+func (a sortByRule) Len() int           { return len(a.objs) }
+func (a sortByRule) Swap(i, j int)      { a.objs[i], a.objs[j] = a.objs[j], a.objs[i] }
+func (a sortByRule) Less(i, j int) bool { return a.rules[[2]int{a.objs[i], a.objs[j]}] }
 
 func part1(input []string) int {
 	rules, i := parseRules(input)
+
 	sum := 0
 
-	for j := i + 1; j < len(input); j++ {
-		p := strings.Split(input[j], ",")
-		pages := utils.ToInts(p)
-
-		if checkPages(pages, rules) {
-			sum += pages[len(pages)/2]
+	for ; i < len(input); i++ {
+		nums := strings.Split(input[i], ",")
+		sortable := &sortByRule{rules, utils.ToInts(nums)}
+		if sort.IsSorted(sortable) {
+			sum += sortable.objs[len(sortable.objs)/2]
 		}
-
 	}
-
 	return sum
-}
-
-type Node struct {
-	left  *Node
-	right *Node
-	value int
-}
-
-func (n *Node) push(rules map[int][]int, value int) {
-	for _, rule := range rules[value] {
-		if rule == n.value {
-			if n.left == nil {
-				n.left = newNode(value)
-			} else {
-				n.left.push(rules, value)
-			}
-			return
-		}
-	}
-	if n.right == nil {
-		n.right = newNode(value)
-	} else {
-		n.right.push(rules, value)
-	}
-}
-
-func newNode(value int) *Node {
-	return &Node{value: value}
-}
-
-func (n *Node) slice() []int {
-	s := []int{}
-	if n.left != nil {
-		s = append(s, n.left.slice()...)
-	}
-	s = append(s, n.value)
-	if n.right != nil {
-		s = append(s, n.right.slice()...)
-	}
-	return s
 }
 
 func part2(input []string) int {
 	rules, i := parseRules(input)
+
 	sum := 0
 
-	for j := i + 1; j < len(input); j++ {
-		p := strings.Split(input[j], ",")
-		pages := utils.ToInts(p)
-
-		if !checkPages(pages, rules) {
-			t := newNode(pages[0])
-			for _, page := range pages[1:] {
-				t.push(rules, page)
-			}
-			sum += t.slice()[len(pages)/2]
+	for ; i < len(input); i++ {
+		nums := strings.Split(input[i], ",")
+		sortable := &sortByRule{rules, utils.ToInts(nums)}
+		if !sort.IsSorted(sortable) {
+			sort.Sort(sortable)
+			sum += sortable.objs[len(sortable.objs)/2]
 		}
-
 	}
-
 	return sum
 }
