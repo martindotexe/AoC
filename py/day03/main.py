0e125d6f8c81e85e14c5ae80c29ff969984aae29
martindotexe <martinss2000@pm.me>
2025-12-04 01:04:45 +0100

    2025-03

--- py/day03/main.py
+++ py/day03/main.py
@@ -4,22 +4,34 @@ def partOne():
         for line in file.readlines():
             line = [int(d) for d in line.strip()]
 
-            l, r = 0, 1
-            max = -1
-            while r < len(line):
-                curr = line[l] * 10 + line[r]
+            tens = max(line[:-1])
+            ones = max(line[line.index(tens) + 1 :])
 
-                if curr > max:
-                    max = curr
-                if line[l] < line[r]:
-                    l = r
+            count += tens * 10 + ones
 
-                r += 1
+        print(count)
 
-            count += max
+
+def partTwo():
+    N = 12
+
+    count = 0
+    with open("in.txt", "r") as file:
+        for line in file.readlines():
+            line = [int(d) for d in line.strip()]
+
+            r = 0
+            for i in range(N - 1, 0, -1):
+                digit = max(line[:-i])
+                line = line[line.index(digit) + 1 :]
+                r = r * 10 + digit
+
+            r = r * 10 + max(line)
+            count += r
 
         print(count)
 
 
 if __name__ == "__main__":
     partOne()
+    partTwo()
