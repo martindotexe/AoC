04ddfaf340a9b8afa439f72bbe6fa080668ab193
martindotexe <martinss2000@pm.me>
2025-12-04 16:36:00 +0100

    2025-04p1

--- py/day04/main.py
+++ py/day04/main.py
@@ -7,8 +7,7 @@ def partOne():
 
     w, h = len(lines[0]), len(lines)
 
-    # number_grid = [[0 for d in range(w)] for _ in range(h)]
-    number_grid = [0 for _ in range(w * h)]
+    neighbor_grid = [-1 for _ in range(w * h)]
 
     def xytoi(x, y):
         return x + y * w
@@ -17,6 +16,9 @@ def partOne():
         for x in range(w):
             if lines[y][x] != C:
                 continue
+
+            neighbor_grid[xytoi(x, y)] += 1
+
             for dx, dy in [(0, 1), (1, -1), (1, 0), (1, 1)]:
                 ix, iy = x + dx, y + dy
                 if ix < 0 or ix >= w or iy < 0 or iy >= h:
@@ -25,18 +27,10 @@ def partOne():
                 if lines[iy][ix] != C:
                     continue
 
-                number_grid[xytoi(x, y)] += 1
-                number_grid[xytoi(ix, iy)] += 1
+                neighbor_grid[xytoi(x, y)] += 1
+                neighbor_grid[xytoi(ix, iy)] += 1
 
-    for i, d in enumerate(map(lambda d: "x" if d < 4 and d != 0 else ".", number_grid)):
-        print(d, end="")
-        if (i + 1) % w == 0:
-            print()
-    # for i, d in enumerate(number_grid):
-    #     print(d, end="")
-    #     if (i + 1) % w == 0:
-    #         print()
-    print(len(list(filter(lambda d: d < 4 and d != 0, number_grid))))
+    print(len(list(filter(lambda d: d < 4 and d >= 0, neighbor_grid))))
 
 
 if __name__ == "__main__":
