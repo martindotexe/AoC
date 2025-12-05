f35c7f1c1d7ec2ab36b25688634997549f36810b
martindotexe <martinss2000@pm.me>
2025-12-05 19:38:07 +0100

    y2025d05

--- py/2025/day05/main.py
+++ py/2025/day05/main.py
@@ -2,18 +2,43 @@ def partOne():
     with open("in.txt", "r") as file:
         fresh, ingredients = file.read().split("\n\n")
 
-        fresh = [tuple(map(int, d.split("-"))) for d in fresh.strip().split("\n")]
-        ingredients = [int(d) for d in ingredients.strip().split("\n")]
+        fresh = [list(map(int, d.split("-"))) for d in fresh.splitlines()]
+        ingredients = list(map(int, ingredients.splitlines()))
 
         count = 0
         for ingredient in ingredients:
-            for f in fresh:
-                if f[0] <= ingredient <= f[1]:
+            for lo, hi in fresh:
+                if lo <= ingredient <= hi:
                     count += 1
                     break
 
         print(count)
 
 
+def partTwo():
+    with open("in.txt", "r") as file:
+        fresh, _ = file.read().split("\n\n")
+
+        fresh = [list(map(int, d.split("-"))) for d in fresh.splitlines()]
+        fresh.sort()
+
+        last = None
+        count = 0
+
+        for lo, hi in fresh:
+            if last is None:
+                last = (lo, hi)
+            elif last[1] < lo:
+                count += last[1] - last[0] + 1
+                last = (lo, hi)
+            else:
+                last = (last[0], max(last[1], hi))
+
+        count += last[1] - last[0] + 1  # type: ignore
+
+        print(count)
+
+
 if __name__ == "__main__":
     partOne()
+    partTwo()
