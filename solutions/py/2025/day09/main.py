ef5f561fad68dd353b16a64022d75541e03a67e2
martindotexe <martinss2000@pm.me>
2025-12-10 12:16:46 +0100

    y2025d09p1: oneliner~

--- solutions/py/2025/day09/main.py
+++ solutions/py/2025/day09/main.py
@@ -5,21 +5,15 @@ def partOne(filepath):
     with open(filepath, "r") as file:
         points = [tuple(map(int, line.split(","))) for line in file.readlines()]
 
-        pointPairs = [
-            (a, b) for a in range(len(points)) for b in range(a + 1, len(points))
-        ]
-
-        largest = 0
-
-        for a, b in pointPairs:
-            x1, y1 = points[a]
-            x2, y2 = points[b]
-
-            area = (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
-            if area > largest:
-                largest = area
-
-        print(largest)
+        print(
+            max(
+                [
+                    (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
+                    for i, (x1, y1) in enumerate(points)
+                    for x2, y2 in points[i:]
+                ]
+            )
+        )
 
 
 if __name__ == "__main__":
