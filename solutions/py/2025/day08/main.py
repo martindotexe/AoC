cbff552e0418026ddba50c127c63baa3d0794552
martindotexe <martinss2000@pm.me>
2025-12-13 23:03:56 +0100

    y2025d8:performance

--- solutions/py/2025/day08/main.py
+++ solutions/py/2025/day08/main.py
@@ -1,69 +1,61 @@
 import sys
-from math import sqrt
 
 
 def distance(p1: list[int], p2: list[int]):
-    return sqrt(sum([(a - b) ** 2 for a, b in zip(p1, p2)]))
+    # Don't need the squre root since we are just comparing distances.
+    # Slower genral n-dimensions version: return sum((a - b) ** 2 for a, b in zip(p1, p2))
+    return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2
 
 
-def partOne(filepath):
+def partOne(points, pairs):
     n = 1000
-    with open(filepath, "r") as file:
-        points = [list(map(int, line.split(","))) for line in file.readlines()]
-        pairs = [(a, b) for a in range(len(points)) for b in range(a + 1, len(points))]
-        pairs.sort(key=lambda x: distance(points[x[0]], points[x[1]]))
 
-        parent = list(range(len(points)))
+    parent = list(range(len(points)))
 
-        def root(x):
-            if parent[x] == x:
-                return x
-            parent[x] = root(parent[x])
-            return parent[x]
+    def root(x):
+        if parent[x] == x:
+            return x
+        parent[x] = root(parent[x])
+        return parent[x]
 
-        def merge(a, b):
-            parent[root(a)] = root(b)
+    def merge(a, b):
+        parent[root(a)] = root(b)
 
-        for a, b in pairs[:n]:
-            merge(a, b)
+    for a, b in pairs[:n]:
+        merge(a, b)
 
-        sizes = [0] * len(points)
+    sizes = [0] * len(points)
 
-        for i in range(len(points)):
-            sizes[root(i)] += 1
+    for i in range(len(points)):
+        sizes[root(i)] += 1
 
-        sizes.sort(reverse=True)
+    sizes.sort(reverse=True)
 
-        print(sizes[0] * sizes[1] * sizes[2])
+    print(sizes[0] * sizes[1] * sizes[2])
 
 
-def partTwo(filepath):
-    with open(filepath, "r") as file:
-        points = [list(map(int, line.split(","))) for line in file.readlines()]
-        pairs = [(a, b) for a in range(len(points)) for b in range(a + 1, len(points))]
-        pairs.sort(key=lambda x: distance(points[x[0]], points[x[1]]))
+def partTwo(points, pairs):
+    parent = list(range(len(points)))
 
-        parent = list(range(len(points)))
+    def root(x):
+        if parent[x] == x:
+            return x
+        parent[x] = root(parent[x])
+        return parent[x]
 
-        def root(x):
-            if parent[x] == x:
-                return x
-            parent[x] = root(parent[x])
-            return parent[x]
+    def merge(a, b):
+        parent[root(a)] = root(b)
 
-        def merge(a, b):
-            parent[root(a)] = root(b)
+    circuits = len(points)
 
-        circuits = len(points)
-
-        for a, b in pairs:
-            if root(a) == root(b):
-                continue
-            merge(a, b)
-            circuits -= 1
-            if circuits == 1:
-                print(points[a][0] * points[b][0])
-                break
+    for a, b in pairs:
+        if root(a) == root(b):
+            continue
+        merge(a, b)
+        circuits -= 1
+        if circuits == 1:
+            print(points[a][0] * points[b][0])
+            break
 
 
 if __name__ == "__main__":
@@ -72,5 +64,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     filepath = sys.argv[1]
-    partOne(filepath)
-    partTwo(filepath)
+    with open(filepath, "r") as file:
+        points = [list(map(int, line.split(","))) for line in file.readlines()]
+        pairs = [(a, b) for a in range(len(points)) for b in range(a + 1, len(points))]
+        # Expensive sort done once for performance
+        pairs.sort(key=lambda x: distance(points[x[0]], points[x[1]]))
+
+        partOne(points, pairs)
+        partTwo(points, pairs)
